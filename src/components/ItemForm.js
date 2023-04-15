fa2aed03baa90e652401d970c7108368e862f2c8
Yehwan Baek <baekokok98@gmail.com>
2023-04-15 14:28:31 +1000

    added code

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({onItemFormSubmit}) {
+  const [name, setName] = useState("");
+  const [category, setCategory] = useState("");
+
+  function handleNameChange(e) {
+    setName(e.target.value);
+  }
+
+  function handleCategoryChange(e) {
+    setCategory(e.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    onItemFormSubmit({
+      id: uuid(),
+      name,
+      category,
+    });
+  }
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" name="name" value={name} onChange={handleNameChange} />
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select name="category" value={category} onChange={handleCategoryChange}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
@@ -24,3 +44,48 @@ function ItemForm(props) {
 }
 
 export default ItemForm;
+
+
+// function ItemForm({onItemFormSubmit}) {
+//   const [formData, setFormData] = useState({
+//    name: "",
+//    category: "",
+//   })
+
+//   function handleChange(e) {
+//     setFormData({
+//       ...formData,
+//       [e.target.name]: e.target.value,
+//     })
+//   }
+
+//   function handleSubmit (e) {
+//     e.preventDefault();
+//     onItemFormSubmit({
+//       id: uuid(),
+//       ...formData
+//     })
+//   }
+
+//   return (
+//     <form className="NewItem" onSubmit={handleSubmit}>
+//       <label>
+//         Name:
+//         <input type="text" name="name" value={formData.name} onChange={handleChange} />
+//       </label>
+
+//       <label>
+//         Category:
+//         <select name="category" value={formData.category} onChange={handleChange}>
+//           <option value="Produce">Produce</option>
+//           <option value="Dairy">Dairy</option>
+//           <option value="Dessert">Dessert</option>
+//         </select>
+//       </label>
+
+//       <button type="submit">Add to List</button>
+//     </form>
+//   );
+// }
+
+
